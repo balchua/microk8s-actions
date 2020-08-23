d9ba8a40ef183f58dc4d989a962f1f2f59144935
balchua <balchua@yahoo.com>
2020-08-23 12:14:59 +0800

    wait for ready.

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,13 @@ async function run() {
   try {
     let channel = core.getInput("channel");
     console.log("creating microk8s group.");
-    await exec.exec("sudo", ["groupadd microk8s"]);
     console.log("install microk8s..")
     await exec.exec("sudo", ["snap", "install", "microk8s", "--channel=" + channel, "--classic"]);
 
     waitForReadyState();
     // Create microk8s group
-    await exec.exec("sudo",["usermod", "-a","-G","microk8s", "$USER"]);
-    await exec.exec("sudo ", ["chown","-f", "-R", "$USER", "~/.kube"]);
+    await exec.exec("sudo",["usermod", "-a","-G","microk8s", "runner"]);
+    await exec.exec("sudo ", ["chown","-f", "-R", "runner", "~/.kube"]);
     
 
     await exec.exec("alias",["kubectl='microk8s kubectl`"]);
