a09e00caed25ec214d2ce89b8c6774b1cb696b30
balchua <balchua@yahoo.com>
2020-08-23 12:12:14 +0800

    wait for ready.

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,8 @@ async function run() {
     await exec.exec("sudo", ["groupadd microk8s"]);
     console.log("install microk8s..")
     await exec.exec("sudo", ["snap", "install", "microk8s", "--channel=" + channel, "--classic"]);
-    await exec.exec("sudo", ["microk8s", "status", "--wait-ready"]);
 
+    waitForReadyState();
     // Create microk8s group
     await exec.exec("sudo",["usermod", "-a","-G","microk8s", "$USER"]);
     await exec.exec("sudo ", ["chown","-f", "-R", "$USER", "~/.kube"]);
@@ -21,6 +21,20 @@ async function run() {
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+async function waitForReadyState() {
+  let ready = false;
+  while (!ready) {
+    try{
+      await exec.exec("sudo", ["microk8s", "status", "--wait-ready"]);
+      ready = true;
+    } catch (err) {
+      console.log("microk8s not yet ready.");
+    }
+  }  
+  
+  
 }
 
 run();
