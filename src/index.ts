7af5f22e6d5cbc5d3f2bcff0bc60dbffd1ea8eb5
balchua <balchua@yahoo.com>
2020-08-23 11:25:35 +0800

    test again.

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,16 @@ import * as exec from '@actions/exec';
 async function run() {
   try {
     let channel = core.getInput("channel");
-    exec.exec("sudo", ["snap", "install", "microk8s", "--channel=" + channel, "--classic"]);
-    // Create microk8s group
-    exec.exec("sudo",["usermod", "-a","-G","microk8s", "$USER"]);
-    exec.exec("sudo ", ["chown","-f", "-R", "$USER", "~/.kube"]);
-    
-    exec.exec("microk8s", ["status", "--wait-ready"]);
+    console.log("install microk8s..")
+    await exec.exec("sudo", ["snap", "install", "microk8s", "--channel=" + channel, "--classic"]);
+    await exec.exec("sudo", ["microk8s", "status", "--wait-ready"]);
 
-    exec.exec("alias",["kubectl='microk8s kubectl`"]);
+    // Create microk8s group
+    await exec.exec("sudo",["usermod", "-a","-G","microk8s", "$USER"]);
+    await exec.exec("sudo ", ["chown","-f", "-R", "$USER", "~/.kube"]);
+    
+
+    await exec.exec("alias",["kubectl='microk8s kubectl`"]);
 
   } catch (error) {
     core.setFailed(error.message);
