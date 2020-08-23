3a3bcb25da688f5d994f7f1bee2d79e6aa8ca690
balchua <balchua@yahoo.com>
2020-08-23 12:45:16 +0800

    wait for ready.

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,8 @@ async function prepareUserEnv() {
   // Create microk8s group
   await exec.exec("sudo",["usermod", "-a","-G","microk8s", "runner"]);
   await exec.exec("mkdir -p", ["/home/runner/.kube"])
-  await exec.exec("sudo ", ["chown","-f", "-R", "runner", "~/.kube"]);
+  await exec.exec("sudo",["microk8s", "kubectl config view --raw > /home/runner/.kube/config"])
+  await exec.exec("sudo ", ["chown","-f", "-R", "runner", "home/runner/.kube"]);
 }
 
 function delay(ms: number)
