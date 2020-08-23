afb5d943c225e2af87843849ce6cf78cda538c08
balchua <balchua@yahoo.com>
2020-08-23 11:16:02 +0800

    test install.

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 
 
-const ChannelInput: string = "channel"; 
 async function run() {
   try {
-    exec.exec("sudo", ["snap", "install", "microk8s", "--channel="+ChannelInput, "--classic"]);
+    let channel = core.getInput("channel");
+    exec.exec("sudo", ["snap", "install", "microk8s", "--channel=" + channel, "--classic"]);
+    // Create microk8s group
+    exec.exec("sudo",["usermod", "-a","-G","microk8s", "$USER"]);
+    exec.exec("sudo ", ["chown","-f", "-R", "$USER", "~/.kube"]);
+    
+    exec.exec("microk8s", ["status", "--wait-ready"]);
+
   } catch (error) {
     core.setFailed(error.message);
   }
